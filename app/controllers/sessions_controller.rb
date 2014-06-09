abb013df60eabb950d0a6656e1edea7764ad6f9e
roohi <roohi@jeronone.com>
2014-06-09 11:52:38 +0530

    change

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,46 @@
+class SessionsController < ApplicationController
+
+
+def new
+authenticate if params[:shop].present?
+end
+def create
+authenticate
+end
+
+def show
+if response = request.env['omniauth.auth']
+sess = ShopifyAPI::Session.new(params[:shop], response['credentials']['token'])
+session[:shopify] = sess
+ShopifyAPI::Base.activate_session(sess)
+
+flash[:notice] = "Logged in"
+redirect_to return_address
+else
+flash[:error] = "Could not log in to Shopify store."
+redirect_to :action => 'new'
+end
+end
+
+protected
+
+def authenticate
+if shop_name = sanitize_shop_param(params)
+redirect_to "/auth/shopify?shop=#{shop_name}"
+else
+redirect_to return_address
+end
+end
+
+
+def return_address
+session[:return_to] || root_url
+end
+
+def sanitize_shop_param(params)
+return unless params[:shop].present?
+name = params[:shop].to_s.strip
+name += '.myshopify.com' if !name.include?("myshopify.com") && !name.include?(".")
+name.sub('https://', '').sub('http://', '')
+end
+end
